e231334d33ce4af89b63c602d1857a1b0b2085a3
nicholas132000 <beefnlemon@gmail.com>
2022-05-23 14:37:16 +0800

    removed tab navigator stuff

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { supabaseClient } from './supabaseClient';
 import { Session } from '@supabase/supabase-js'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoginPage from './components/LoginPage';
 import CreateAccountPage from './components/CreateAccountPage';
@@ -11,16 +12,7 @@ import HomePage from './components/HomePage';
 import TodoList from './components/TodoListPage';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 
-function tabBar() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomePage({navigation})} /> //note to nic if cant work, try component={HomePage}
-      <Tab.Screen name="TodoList" component={TodoList} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   const [session, setSession] = useState(null)
@@ -45,7 +37,7 @@ export default function App() {
               <Stack.Screen name="CreateAccount" component={CreateAccountPage} />
             </>
             ) : (
-              <Stack.Screen name="Home" component={tabBar} />
+              <Stack.Screen name="Home" component={HomePage} />
             )}
         </Stack.Navigator>
     </NavigationContainer>
