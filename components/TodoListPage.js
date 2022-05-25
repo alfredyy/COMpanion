aef767fc9b4f243dd016d1309860493693b18523
alfredyy <alfredyiow@gmail.com>
2022-05-25 18:40:12 +0800

    TDPage, incomplete

--- components/TodoListPage.js
+++ components/TodoListPage.js
@@ -1,38 +1,84 @@
-import { StyleSheet, Text, View, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import React, {useState} from 'react';
-import TodoItem from './TodoListPage1';
-
+import {View, StyleSheet, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {Button, Input, ListItem, CheckBox, Text} from 'react-native-elements';
+import React, {useEffect, useRef, useState} from 'react';
+import { supabaseClient } from '../supabaseClient';
+import 'react-native-url-polyfill/auto'
 
 export default function TodoList() {
-  const[todos, setTodos] = useState([
-    { text: 'dummytask1', key: '1' },
-    { text: 'dummytask2', key: '2' },
-    { text: 'dummytask3', key: '3' }
-  ]);
+  const [todos, setTodos] = useState([]);
+  const componentMounted = useRef(true);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const {data, error} = await supabaseClient
+        .from('todos')
+        .select('*')
+        .order('id', {ascending: false});
+      if (error) {
+        console.log('error', error);
+      } else {
+        console.log('Todos: ', data);
+        setTodos(data);
+      }
+    };
+    if (componentMounted.current) {
+      fetchTodos();
+    }
+
+    return () => {
+      componentMounted.current = false;
+    };
+  }, []);
+
+  const toggleCompleted = async (id, completed) => {
+    const {data, error} = await supabaseClient
+      .from('todos')
+      .update({completed: !completed})
+      .eq('id', id)
+      .single();
+    if (error) {
+      console.log(error);
+    } else {
+      setTodos(todos.map(todo => (todo.id === id ? data : todo)));
+    }
+  };
+
+  const deleteTodo = async id => {
+    const {error} = await supabaseClient.from('todos').delete().eq('id', id);
+    if (error) {
+      console.log('error', error);
+    } else {
+      setTodos(todos.filter(x => x.id !== Number(id)));
+    }
+  };
+
 
   return (
     <TouchableWithoutFeedback onPress={() => {
       Keyboard.dismiss();
     }}>
     <View style={styles.container}>
-
-      <View style={styles.header}>
-            <Text style={styles.title}>To Do List</Text>
-      </View>
-
-     
-
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} />
-            )}
-          />
-        </View>
-
-      
-  
+    <FlatList
+          scrollEnabled={true}
+          data={todos}
+          keyExtractor={item => `${item.id}`}
+          renderItem={({item: todo}) => (
+            <ListItem bottomDivider>
+              <ListItem.Content>
+                <View style={[styles.dFlex]}>
+                  <CheckBox
+                    checked={todo.completed}
+                    onPress={() => toggleCompleted(todo.id, todo.completed)}
+                  />
+                  <Text h4 style={[styles.mtAuto]}>
+                    {todo.item_name}
+                  </Text>
+                  <Button title="Delete" onPress={() => deleteTodo(todo.id)} />
+                </View>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
     </View>
     </TouchableWithoutFeedback>
   );
@@ -41,38 +87,22 @@ export default function TodoList() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ec2929',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'Roboto'
+    marginTop: 40,
   },
-
-  list: {
-    backgroundColor: 'white',
-    flex: 1,
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+  },
+  mt20: {
     marginTop: 20,
-    marginBottom: 100,
-    borderRadius: 25,
-    paddingTop: 20,
-    paddingLeft: 15,
+  },
+  mtAuto: {
+    margin: 'auto',
+  },
+  dFlex: {
+    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-   
   },
-  header: {
-    justifyContent: 'center',
-    marginTop: 30,
-    paddingLeft: 100,
-    paddingRight: 100,
-    flex: 0.13,
-    backgroundColor: 'white',
-    borderRadius: 13,
- },
- title: {
-    textAlign: 'center',
-    fontSize: 25,
-    fontWeight: 'bold',
- 
- }
 });
